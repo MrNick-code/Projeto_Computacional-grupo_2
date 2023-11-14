1452dba8b192c6e4d9b1c3f65f33cd8aea6badc8
Matheus Capelin <62571818+MrNick-code@users.noreply.github.com>
2023-11-14 01:46:59 -0300

    Aplicação de Criptografia Assimétrica.
    Inacabado.

---
+++ cripto_assimetrica.py
@@ -0,0 +1,30 @@
+##### Criptografia/Descriptografia Assimétrica usando Algorítimos Modernos ######
+'''
+### Criptografia Assimétrica
+# ECC (Elliptic Curve Cryptography):
+
+	A ECC é baseada em operações matemáticas envolvendo curvas elípticas sobre campos finitos.
+
+	A chave pública em ECC é gerada como um ponto na curva elíptica e a chave privada é um número inteiro.
+
+	A segurança da ECC baseia-se no problema do logaritmo discreto em uma curva elíptica, que é considerado mais difícil de resolver em
+	 comparação com problemas semelhantes em outros campos. A ECC oferece uma segurança equivalente ao RSA com chaves muito menores, 
+	 tornando-a uma escolha atraente para dispositivos com recursos limitados.
+'''
+
+import hashlib
+
+### ECC (implementação)
+
+'''
+Fonte:
+https://medium.com/@schaetzcornelius/learn-how-to-code-elliptic-curve-cryptography-be646d2c9757
+https://www.geeksforgeeks.org/blockchain-elliptic-curve-cryptography/ 
+'''
+
+
+
+'''
+Próximos passo em EEC:
+
+'''
