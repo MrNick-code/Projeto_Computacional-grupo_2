944f977c4dfc48d9657a996ca32344ef8aecdd37
Enzo Benko <[enzobenko@gmail.com]>
2023-12-19 15:38:09 -0300

    Criptografia de Bytes OneTimePad implementada

--- cifras_functions.py
+++ cifras_functions.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-# Ideias para próximos passos: implementar solucao por frequencia, bruteforce cesar, arrumar problemas
+'''
+Criptografia clássica: Dois tipos principais de cifra abrangeram a história da criptografia clássica: as cifras de substituição e as
+de transposição. Nas cifras de substituição os símbolos do alfabeto do texto plano são substituídos por um ou mais símbolos do 
+alfabeto do texto cifrado de acordo com uma regra, gerando o texto cifrado. Já nas cifras de transposição os símbolos do alfabeto do 
+texto plano são permutados, também de acordo com uma regra, gerando o texto cifrado.
+'''
 
 # Cifra de César
 # Altera a letra do alfabeto pulando n letras da sequencia
@@ -184,6 +189,7 @@ def de_transpColumn(texto, key='abc'):
 # É uma cifra polialfabetica que consiste basicamente em pegar uma palavra-chave e aplicar a cifra de César várias vezes, de acordo com os caracteres da palavra-chave.
 # Fonte: https://wiki.imesec.ime.usp.br/books/criptografia/page/cifra-de-vigenère
 
+@staticmethod
 def vigenere(texto, chave='demar'):
     cifrado=""
     numchave=[]
@@ -201,6 +207,7 @@ def vigenere(texto, chave='demar'):
         cifrado += char
     return cifrado
 
+@staticmethod
 def de_vigenere(texto, chave='demar'):
     cifrado=''
     numchave=[]
@@ -220,6 +227,7 @@ def de_vigenere(texto, chave='demar'):
     return cifrado
 
 '''
+#Exemplo de Criptografia clássica:
 og='Hello World'
 cesr=cesar(og,3)
 atbas=atbash(og)
@@ -231,11 +239,54 @@ print(f'\033[35;1mKey:\033[m demar')
 print(f'\033[36;1mTexto Original:\033[m {og}')
 print(f'\033[35;1mTexto Criptogrado com César (n=3):\033[m {cesr}')
 print(f'\033[35;1mTexto Criptogrado com AtBash:\033[m {atbas}')
-print(f'\033[35;1mTexto Criptogrado com Transposição de Coluna (n=3):\033[m {TranC}')
+print(f'\033[35;1mTexto Criptogrado com Transposição de Coluna:\033[m {TranC}')
 print(f'\033[35;1mTexto Criptogrado com Vigenère:\033[m {vig}')
 print(f'\033[36;1mTexto Descriptogrado com César:\033[m {de_cesar(cesr,3)}')
 print(f'\033[36;1mTexto Descriptogrado com AtBash:\033[m {de_atbash(atbas)}')
 print(f'\033[36;1mTexto Descriptogrado com Transposição de Coluna:\033[m {de_transpColumn(TranC,"demar")}')
 print(f'\033[36;1mTexto Descriptogrado com Vigenère:\033[m {de_vigenere(vig)}')
+print('==============================================================')
+'''
+'''
+Com a entrada da computação no cenário e a chegada da era moderna, a criptografia expandiu seus campos de atuação, de modo que hoje
+lidamos com sequencias de bits, que podem significar letras, pixels de uma imagem, áudios, entre outros. Logo, a Criptografia Moderna
+trata da segurança da informação que essas sequencias de bits carregam.
+'''
+# One time pad
+# Utiliza da operação XOR , bit a bit, com uma chave para obter uma mensagem criptografada. A funcao foi definida tendo em mente informacao
+# em modelos de bytes
+@staticmethod
+def onetimepad(plaintext, key, encoding='utf-8'):
+    ciphertext=bytes()
+    
+    if isinstance(plaintext, bytes) == False:
+        plaintext=bytes(plaintext, encoding)
+    
+    if isinstance(key, bytes) == False:
+        key=bytes(key, encoding)
+    
+    while len(key) <= len(plaintext):
+        key += key
+
+    for p, k in zip( plaintext, key ):
+        xor = p ^ k
+        ciphertext += bytes([xor])
+
+    return ciphertext
+
+@staticmethod
+def de_onetimepad(ciphertext, key, encoding='utf-8'):
+    return onetimepad(ciphertext, key, encoding)
+
+'''
+#Exemplo de Criptografia moderna:
+texto = b'Hello World!'
+chave = b'demar'
+
+print('==============================================================')
+print(f'\033[35;1mKey em bytes:\033[m {chave}')
+print(f'\033[36;1mBytes Original:\033[m {texto}')
+print(f"\033[35;1mBytes Criptogrado com OneTimePad:\033[m {onetimepad(texto, chave)}")
+print(f"\033[36;1mBytes Descriptogrado com OneTimePad:\033[m {de_onetimepad(onetimepad(texto, chave), chave)}")
 print('==============================================================')
 '''
